6da7bff40346c20aedbca89c69421db48eebb59c
JustisSmith <smithj59@vcu.edu>
2023-11-21 23:11:02 -0500

    test -- changed level1 layout

--- settings.py
+++ settings.py
@@ -1,15 +1,15 @@
 level1_map = ['                            ',
               '                            ',
               '                            ',
-              ' XX    XXX             XX   ',
-              ' XX                         ',
-              ' XXXX          XX         XX',
-              ' XXXX        XX             ',
-              ' XX     X  XXXX     XX  XX  ',
+              'XX     XXX             XX   ',
+              'XX                          ',
+              'XXXX           XX         XX',
+              'XXXXX        XX             ',
+              'XX         XXXX     XX  XX  ',
               '        X  XXXX     XX  XXX ',
               '     XXXX  XXXXXX   XX  XXXX',
               'XXXXXXXXXXXXXXXXXXXXXXXXXXXX']
 
 tile_size = 64
-screen_width = 1200
+screen_width = 1600
 screen_height = len(level1_map) * tile_size
