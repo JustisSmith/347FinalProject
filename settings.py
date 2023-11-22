0b5855c19cf447b9bfe98ace5e228d5f51bc2486
JustisSmith <149518111+JustisSmith@users.noreply.github.com>
2023-11-21 22:46:48 -0500

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,15 @@
+level1_map = ['                            ',
+              '                            ',
+              '                            ',
+              ' XX    XXX             XX   ',
+              ' XX                         ',
+              ' XXXX          XX         XX',
+              ' XXXX        XX             ',
+              ' XX     X  XXXX     XX  XX  ',
+              '        X  XXXX     XX  XXX ',
+              '     XXXX  XXXXXX   XX  XXXX',
+              'XXXXXXXXXXXXXXXXXXXXXXXXXXXX']
+
+tile_size = 64
+screen_width = 1200
+screen_height = len(level1_map) * tile_size
