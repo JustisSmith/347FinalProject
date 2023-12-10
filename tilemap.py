4515e470d7522af6b21949abe7e9a7383c0694eb
JustisSmith <smithj59@vcu.edu>
2023-12-10 08:14:26 -0500

    nothing changed

--- tilemap.py
+++ tilemap.py
@@ -72,7 +72,7 @@ class Tilemap:
                 loc = str(x) + ';' + str(y)
                 if loc in self.tilemap:
                     tile = self.tilemap[loc]
-                    surf.blit(self.game.assets[tile['type']][tile['variant']], (tile['pos'][0] * self.tile_size - offset[0], tile['pos'][1] * self.tile_size - offset[1]))   # puts a tile on the display
+                    surf.blit(self.game.assets[tile['type']][tile['variant']], (tile['pos'][0] * self.tile_size - offset[0], tile['pos'][1] * self.tile_size - offset[1]))   # puts a tile on the dislpay
                     #pygame.draw.rect(surf, (255,0,0), (tile['pos'][0] * self.tile_size - offset[0], tile['pos'][1] * self.tile_size - offset[1], self.tile_size, self.tile_size), width=2)
                     
 
